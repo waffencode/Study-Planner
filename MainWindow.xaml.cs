5d5aa8df907b5aaa9b7c79b73fb5522833740e5d
waffencode <waffencode@gmail.com>
2022-09-08 01:52:16 +0300

    Операция получения контекста данных кнопки вынесена в отдельный метод

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using Study_Planner.Components;
 
 namespace Study_Planner
 {
@@ -9,14 +10,14 @@ namespace Study_Planner
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Components.TasksCollection MainTasksCollection;
-        public ObservableCollection<Components.Task> Tasks;
+        public TasksCollection MainTasksCollection;
+        public ObservableCollection<Task> Tasks;
 
         public MainWindow()
         {
             InitializeComponent();
-            MainTasksCollection = new Components.TasksCollection();
-            Tasks = new ObservableCollection<Components.Task>();
+            MainTasksCollection = new TasksCollection();
+            Tasks = new ObservableCollection<Task>();
             TasksList.DataContext = Tasks;
         }
 
@@ -27,16 +28,17 @@ namespace Study_Planner
 
         private void ToggleCompleted_Click(object sender, RoutedEventArgs e)
         {
-            Button ToggleCompletedButton = (Button)sender;
-            Components.Task SelectedTask = (Components.Task)ToggleCompletedButton.DataContext;
-            SelectedTask.ToggleCompletedState();
+            GetButtonDataContext((Button)sender).ToggleCompletedState();
         }
 
         private void DeleteTask_Click(object sender, RoutedEventArgs e)
         {
-            Button DeleteTaskButton = (Button)sender;
-            Components.Task SelectedTask = (Components.Task)DeleteTaskButton.DataContext;
-            Tasks.Remove(SelectedTask);
+            Tasks.Remove(GetButtonDataContext((Button)sender));
+        }
+
+        private Task GetButtonDataContext(Button button)
+        {
+            return (Task)button.DataContext;
         }
     }
 }
