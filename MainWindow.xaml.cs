0f7298a30464ce1c6192532cbdff1f1ce84e5229
waffencode <waffencode@gmail.com>
2022-09-07 22:46:12 +0300

    Удалены лишние директивы using

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,6 @@
-﻿using Study_Planner.Components;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Study_Planner
 {
