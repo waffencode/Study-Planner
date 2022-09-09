fe49a2b9d58fad5b5ed2c2993283a744b59e203c
waffencode <waffencode@gmail.com>
2022-09-09 16:43:44 +0300

    Запись свойств изменена на более компактную

--- Components/Task.cs
+++ Components/Task.cs
@@ -7,51 +7,19 @@ namespace Study_Planner.Components
     public class Task : INotifyPropertyChanged
     {
         private int _id;
+        public int Id { get => _id; set => _id = value; }
+
         private string _shortDescription;
-        private bool _isCompleted;
+        public string ShortDescription { get => _shortDescription; set => _shortDescription = value; }
+
+        private bool _isCompleted = false;
+        public bool IsCompleted { get => _isCompleted; set { _isCompleted = value; NotifyPropertyChanged(); } }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public int Id 
-        { 
-            get
-            {
-                return _id;
-            } 
-            set
-            {
-                _id = value;
-            } 
-        }
-
-        public string ShortDescription 
-        { 
-            get 
-            {
-                return _shortDescription;
-            } 
-            set 
-            {
-                _shortDescription = value;
-            } 
-        }
-
-        public bool IsCompleted 
-        {
-            get 
-            {
-                return _isCompleted;
-            }
-            set 
-            { 
-                _isCompleted = value;
-                NotifyPropertyChanged();
-            } 
         }
 
         public Task(int givenID, string givenDescription)
